7d2e847058748bfd0c578a18fb088059d23df655
gazi <rafalgazda.mail@gmail.com>
2018-10-07 23:02:05 +0200

    Dice app almost finished

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var deploy = require('gulp-gh-pages');
+var mode = require('gulp-mode')();
+/*var uglify = require('gulp-uglify');*/
+/**
+ * Push build to gh-pages
+ */
+
+ 
+gulp.task('dist', function() {
+  gulp.src('src/*.js')
+      .pipe(mode.production(uglify()))
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
+gulp.task('reload', function() {
+    browserSync.reload();
+});
+
+gulp.task('serve', ['sass'], function() {  
+    browserSync({
+        server: 'src'
+    });
+    
+    gulp.watch('src/*.html', ['reload']);
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('sass', function() {
+    return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('font-awesome', () => {
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+  .pipe(gulp.dest('src/css'));
+})
+
+gulp.task('fonts', () => {
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('src/fonts'));
+});
+
+gulp.task('default', ['serve', 'font-awesome', 'fonts']);
